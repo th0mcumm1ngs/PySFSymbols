f414c6c7b5e63816b8945d41c91fe86be89fe829
Thōm Cummings <thom-cummings@outlook.com>
2025-07-09 02:23:50 +0100

    feat: Add library initialisation

---
+++ pysfsymbols/__init__.py
@@ -0,0 +1,7 @@
+"""
+PySFSymbols - A library for implementing SF Symbols within Python GUI applications.
+"""
+
+from .sf_symbols import fetch_symbol, fetch_all_symbols, fetch_all_names, fetch_all
+
+__all__ = ["fetch_symbol", "fetch_all_symbols", "fetch_all_names", "fetch_all"]
